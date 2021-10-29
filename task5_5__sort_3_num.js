43b0494507befa8c44424cc67add4b5938a44dbb
Olya Lucky <brad.groove@gmail.com>
2021-10-29 10:22:17 +0300

    Added 5 task

--- task5_5__sort_3_num.js
+++ task5_5__sort_3_num.js
@@ -1,10 +1,27 @@
-// Task 4.5:enter two positive numbers and check if 1st number is greater than 2nd one
-const prompt = require('prompt-sync')({sigint: true});
-
-const num1 = prompt('Please enter 1st number: ');
-const num2 = prompt('Please enter 2nd number: ');
-if(num1 > num2){
-    console.log("1st number is greater than 2nd one");
-}else{
-console.log("1st number is less than or equal to 2nd one");
-}
+// Task 5.5: Write a JavaScript function with conditional statement to sort
+// three numbers
+function sort3num(a,b,c){
+    if (a > b && a > c) {
+      if (b > c) {
+          return a + ", " + b + ", " + c;
+      } else {
+          return a + ", " + c + ", " + b;
+      }
+    } else if (b > a && b > c) {
+      if (a > c) {
+          return b + ", " + a + ", " + c;
+      } else {
+          return b + ", " + c + ", " + a;
+      }
+    } else if (c > a && c > b) {
+      if (a > b) {
+          return c + ", " + a + ", " + b;
+      } else {
+          return c + ", " + b + ", " + a;
+      }
+    }
+  }
+  
+  console.log(sort3num(19,4,21)); // 21, 19, 4
+  console.log(sort3num(-10,42,26)); // 42, 26, -10
+  console.log(sort3num(5,6,7)); // 7, 6, 5
